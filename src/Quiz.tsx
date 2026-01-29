88435e5a87778cc60e0697d1116e27e27d63547f
developcoding <omidvarisl@gmx.de>
2026-01-29 14:34:14 +0100

    feat: calculate quiz score and allow restart

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -4,13 +4,14 @@ import {questions} from './questions';
 
 const Quiz = () => {
 
+    const [score, setScore] = useState<number>(0)
     const [selectedOption, setSelectedOption] = useState<string>('');
     const [currentQuestion, setCurrentQuestion] = useState<number>(0);
     const [isFinal, setIsFinal] = useState<boolean>(false);
     const handelAnswer = (option:string)=>{
         setSelectedOption(option)
        if(option === questions[currentQuestion].answer){
-            console.log(option)
+            setScore(score+1)
         }
         setTimeout(
             ()=>{
@@ -22,10 +23,21 @@ const Quiz = () => {
             }
             ,500)
     }
+     const windowRefresh =()=>{
+         window.location.reload();
+    }
   return (
     <div className="w-full">
-        <div className="w-full flex flex-col items-center p-3 gap-2 font-poppins">
-            <h3 className="text-2xl font-bold">{questions[currentQuestion].question}</h3>
+        {
+            isFinal?(
+                <div>
+                    <h2 className='text-3xl font-bold p-3'>Quiz completed</h2>
+                    <p className='text-xl font-bold p-3'>Your score is: {score} from {questions.length}</p>
+                    <button className='bg-blue-500 text-white p-3 rounded-xl cursor-pointer mt-2 mb-2 hover:bg-blue-600 transition-all duration-300' onClick={windowRefresh}>Refresh</button>
+                </div>
+            ):(
+                <div className="w-full flex flex-col items-center p-3 gap-2 font-poppins">
+            <h3 className="text-2xl font-bold">{`${currentQuestion+1}: ${questions[currentQuestion].question}`}</h3>
             <ul className="w-full flex flex-col justify-center items-center gap-2">
 
                 {
@@ -35,16 +47,13 @@ const Quiz = () => {
                                 onClick={()=>handelAnswer(item)}
                                 key={index} className="w-full text-left font-bold text-lg hover:bg-gray-700 p-2 rounded-lg cursor-pointer transition-all duration-300 hover:text-white">{item}</li>
                         )
-                            
-                        
                     )
-                }
-
-
-                
-                
+                } 
             </ul>
         </div>
+            )
+        }
+        
     </div>
   )
 }
