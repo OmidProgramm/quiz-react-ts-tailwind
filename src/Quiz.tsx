9b53c29586402859a9ee35443c6b2db46166f7a5
developcoding <omidvarisl@gmx.de>
2026-01-29 13:14:50 +0100

    feat: display quiz question and options

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -1,14 +1,23 @@
-
+import {questions} from './questions';
 
 const Quiz = () => {
   return (
     <div className="w-full">
         <div className="w-full flex flex-col items-center p-3 gap-2 font-poppins">
-            <h3 className="text-2xl font-bold">question 1</h3>
-            <ul className="w-full flex flex-col gap-2">
-                <li className="w-full text-center font-bold text-lg hover:bg-gray-700 p-2 rounded-lg cursor-pointer transition-all duration-300 hover:text-white">option 1</li>
-                <li className="w-full text-center font-bold text-lg hover:bg-gray-700 p-2 rounded-lg cursor-pointer transition-all duration-300 hover:text-white">option 1</li>
-                <li className="w-full text-center font-bold text-lg hover:bg-gray-700 p-2 rounded-lg cursor-pointer transition-all duration-300 hover:text-white">option 1</li>
+            <h3 className="text-2xl font-bold">{questions[0].question}</h3>
+            <ul className="w-full flex flex-col justify-center items-center gap-2">
+
+                {
+                    questions[0].options.map(
+                        (item:string,index:number)=>
+                            <li key={index} className="w-full text-left font-bold text-lg hover:bg-gray-700 p-2 rounded-lg cursor-pointer transition-all duration-300 hover:text-white">{item}</li>
+                        
+                    )
+                }
+
+
+                
+                
             </ul>
         </div>
     </div>
